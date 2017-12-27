bd09c351ada315661b16fa91248e95bc93262e08
Jesse L Lawson <homestarflyer@gmail.com>
2017-12-27 03:10:09 -0700

    made change to colors

--- src/pages/1.js
+++ src/pages/1.js
@@ -6,7 +6,7 @@ export default ({ transition }) => (
     <h1>Gatsby, is it great?</h1>
 
     <small>An open source  project, <br />
-    <span><i>static site generator</i>  <bold>||</bold>  <i>javascript web framework</i> <bold>||</bold> <i>PWA generator</i></span><br /><br />
+    <span><i>static site generator</i>  <bold> || </bold>  <i>javascript web framework</i> <bold> || </bold> <i>PWA generator</i></span><br /><br />
     Built with React, Webpack, graphQL. 1.0.0 released in JULY 2017</small>
   </div>
 );

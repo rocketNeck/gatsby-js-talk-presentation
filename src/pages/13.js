4dc323528dd3c9a0f36512669bb98e6325ffc01b
Jesse L Lawson <homestarflyer@gmail.com>
2017-12-27 14:12:56 -0700

    ran build

--- src/pages/13.js
+++ src/pages/13.js
@@ -3,6 +3,6 @@ import Link from 'gatsby-link';
 
 export default ({ transition }) => (
   <div style={transition && transition.style}>
-    <h1>Ecosystem || Where does it live? || Buzzwords!</h1>
+    <h1>Ecosystem || Where does it come from? || Buzzwords!</h1>
   </div>
 );
